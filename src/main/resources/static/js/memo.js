512678d56a7053bd7f1bd9294bea4d8d247b9f11
im-RYamaguchi <r.yamaguchi@infomart.co.jp>
2025-06-02 16:35:12 +0900

    JSでレスポンスを処理する

--- src/main/resources/static/js/memo.js
+++ src/main/resources/static/js/memo.js
@@ -1,3 +1,18 @@
+const buildHTML = (XHR) => {
+  const item = XHR.response;
+  const html = `
+    <div class="post">
+      <div class="post-date">
+        投稿日時：${item.createdAt}
+      </div>
+      <div class"post-content">
+        ${item.content}
+      </div>
+    </div>
+  `;  
+  return html;
+}
+
 function post(){
   // リクエストを送信する処理
   console.log("post OK");
@@ -19,6 +34,20 @@ function post(){
     XHR.responseType = "json";
     // フォーム内容をコントローラーへ送信
     XHR.send(formData);
+
+    // リクエストの送信が成功したとき
+    XHR.onload = () => {
+      // リクエストが失敗したとき
+      if(XHR.status != 200){
+        alert(`Error ${XHR.status}： ${XHR.response.error}`);
+        return null;
+      }
+      const list = document.getElementById("list");
+      const formText = document.getElementById("content");
+
+      list.insertAdjacentHTML("afterend", buildHTML(XHR));
+      formText.value = "";
+    };
   });
 }
 
